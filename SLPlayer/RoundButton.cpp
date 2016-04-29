20e1dbe8647c1f62bb56dc714b5dce940acd4d4a
chao <531236305@qq.com>
2016-04-29 17:09:49 +0800

    添加RoundButton类

---
+++ SLPlayer/RoundButton.cpp
@@ -0,0 +1,48 @@
+#include "RoundButton.h"
+
+
+
+RoundButton::RoundButton()
+{
+	m_eventToAction[PRESS_DOWN] = nullptr;
+	m_eventToAction[POP_UP] = nullptr;
+	m_eventToAction[DOWN] = nullptr;
+}
+
+
+RoundButton::~RoundButton()
+{
+}
+
+
+// 鼠标移动到本按钮上
+void RoundButton::mouseMoveIn()
+{
+	m_state = UP_HIGHLIGHT;
+}
+
+
+// 鼠标移动到本按钮外
+void RoundButton::mouseMoveOut()
+{
+	m_state = UP;
+}
+
+
+// 按钮被按下
+void RoundButton::pressDown()
+{
+	m_state = DOWN;
+}
+
+
+// 按钮弹起
+void RoundButton::up()
+{
+	m_state = UP_HIGHLIGHT;
+	
+	if (m_eventToAction[CLICK] != nullptr)
+	{
+		(m_eventToAction[CLICK])();
+	}
+}
---
+++ SLPlayer/RoundButton.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+#include <map>
+#include <functional>
+
+class RoundButton
+{
+public:
+	RoundButton();
+	~RoundButton();
+
+	// 鼠标移动到本按钮上
+	void mouseMoveIn();
+	// 鼠标移动到本按钮外
+	void mouseMoveOut();
+
+	// 按钮被按下
+	void pressDown();
+
+	// 按钮弹起
+	void up();
+
+	// 连接事件和响应动作
+
+public:
+	// 按钮事件：按下、弹起、点击
+	enum Event
+	{
+		PRESS_DOWN, POP_UP, CLICK
+	};
+	// 按钮的状态：弹起，弹起+鼠标移入，按下
+	enum State
+	{
+		UP, UP_HIGHLIGHT, DOWN
+	};
+
+private:
+	State m_state = UP; // 按钮的状态
+	std::map<Event, std::function<void>> m_eventToAction; // 事件和响应动作之间的映射
+};
+
