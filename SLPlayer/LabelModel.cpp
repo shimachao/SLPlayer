3352e6ba6b8d1d876ca31780ef0683f025bf4b26
chao <531236305@qq.com>
2016-05-04 10:19:45 +0800

    添加LabelModel类。

---
+++ SLPlayer/LabelModel.cpp
@@ -0,0 +1,92 @@
+#include "LabelModel.h"
+
+
+
+LabelModel::LabelModel()
+{
+}
+
+
+LabelModel::~LabelModel()
+{
+}
+
+
+// 设置文本
+void LabelModel::set_text(std::wstring text)
+{
+	m_text = text;
+}
+
+
+// 添加字符，在光标所在位置添加
+void LabelModel::add_char(wchar_t c)
+{
+	m_text.insert(m_cursor_pos, 1, c);
+}
+
+
+// 转为编辑状态
+void LabelModel::turn_to_edit()
+{
+	m_state = State::EDIT;
+}
+
+
+// 转为显示状态
+void LabelModel::turn_to_display()
+{
+	m_state = State::DISPLAY;
+	m_selected_star_pos = m_selected_end_pos = 0;
+}
+
+
+// 查询状态
+LabelModel::State LabelModel::get_state() const
+{
+	return m_state;
+}
+
+
+
+// 设置光标位置
+void LabelModel::set_cursor_pos(std::wstring::size_type pos)
+{
+	if (pos >= 0 && pos <= m_text.size())
+	{
+		m_cursor_pos = pos;
+	}
+}
+
+
+// 查询光标位置
+std::wstring::size_type LabelModel::get_cursor_pos() const
+{
+	return m_cursor_pos;
+}
+
+
+// 设置被选中字符的范围
+void LabelModel::set_selected_range(std::wstring::size_type start, std::wstring::size_type end)
+{
+	if (start >= 0 && end <= m_text.size() && start < end)
+	{
+		m_selected_star_pos = start;
+		m_selected_end_pos = end;
+	}
+}
+
+
+// 查询被选中文本的范围
+void LabelModel::get_selected_range(std::wstring::size_type &start, std::wstring::size_type &end) const
+{
+	start = m_selected_star_pos;
+	end = m_selected_end_pos;
+}
+
+
+// 取消选中操作
+void LabelModel::deselect()
+{
+	m_selected_star_pos = m_selected_end_pos = 0;
+}
---
+++ SLPlayer/LabelModel.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+
+class LabelModel
+{
+public:
+	LabelModel();
+	~LabelModel();
+
+	// 设置文本
+	void set_text(std::wstring text);
+	// 添加字符，在光标所在位置添加
+	void add_char(wchar_t c);
+
+	// 转为编辑状态
+	void turn_to_edit();
+	// 转为显示状态
+	void turn_to_display();
+	// 查询状态
+	enum class State;
+	State get_state() const;
+
+
+	// 设置光标位置
+	void set_cursor_pos(std::wstring::size_type pos);
+	// 查询光标位置
+	std::wstring::size_type get_cursor_pos() const;
+
+	// 设置被选中字符的范围
+	void set_selected_range(std::wstring::size_type start, std::wstring::size_type end);
+	// 查询被选中文本的范围
+	void get_selected_range(std::wstring::size_type &start, std::wstring::size_type &end) const;
+	// 取消选中操作
+	void deselect();
+
+
+	//-----------------定义类型信息------------------
+public:
+	// label的状态：显示、编辑
+	enum class State
+	{
+		DISPLAY, EDIT
+	};
+
+private:
+	std::wstring m_text; // label的文本
+	State m_state = State::DISPLAY; // label的状态，默认处于display状态
+	std::wstring::size_type m_cursor_pos = 0; // 光标位置
+	std::wstring::size_type m_selected_star_pos = 0; // 被选中文本的开始位置
+	std::wstring::size_type m_selected_end_pos = 0; // 被选中文本的结束位置
+};
+
