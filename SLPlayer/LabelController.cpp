3ba20d02621d91fc372aefdd7b4fcf94c5cbed32
chao <531236305@qq.com>
2016-05-04 11:39:48 +0800

    添加LabelController类

---
+++ SLPlayer/LabelController.cpp
@@ -0,0 +1,13 @@
+#include "LabelController.h"
+
+
+
+LabelController::LabelController(int left, int top, int width, int height)
+	:m_left(left), m_top(top), m_width(width), m_height(height)
+{
+}
+
+
+LabelController::~LabelController()
+{
+}
---
+++ SLPlayer/LabelController.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "LabelModel.h"
+
+class LabelController
+{
+public:
+	LabelController(int left, int top, int width, int height);
+	~LabelController();
+
+	// 设置model
+	void set_model(LabelModel *model);
+
+	// 击中测试
+	bool hit_test(int x, int y) const;
+	// 光标移出
+	void mouse_move_out();
+	// 光标移入
+	void mouse_move_in();
+	// 光标移动消息
+	void mouse_move_over();
+	// 鼠标左键按下
+	void left_button_down();
+	// 鼠标左键弹起
+	void left_button_up();
+
+private:
+	int m_left;
+	int m_top;
+	int m_width;
+	int m_height;
+
+	LabelModel *m_model = nullptr;
+};
+
