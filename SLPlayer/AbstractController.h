e79033963faa03a8ecad5f0ffbe8c4aa64dd8f32
chao <531236305@qq.com>
2016-05-05 10:18:49 +0800

    添加AbstractController

---
+++ SLPlayer/AbstractController.h
@@ -0,0 +1,19 @@
+#pragma once
+class AbstractController
+{
+public:
+	AbstractController();
+	~AbstractController();
+
+	// 击中测试
+	virtual bool hit_test(int x, int y) const = 0;
+	// 光标移出
+	virtual void mouse_move_out() = 0;
+	// 光标移入
+	virtual void mouse_move_in() = 0;
+	// 鼠标左键按下
+	virtual void left_button_down() = 0;
+	// 鼠标左键弹起
+	virtual void left_button_up() = 0;
+};
+
