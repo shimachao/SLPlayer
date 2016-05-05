dba11d01dc21d841c3ebc920f4f99f1180a07a1d
chao <531236305@qq.com>
2016-05-05 11:52:18 +0800

    添加Progress类。Progress组合ProgressView、ProgressController、ProgressModel

---
+++ SLPlayer/Progress.cpp
@@ -0,0 +1,84 @@
+#include "Progress.h"
+
+
+
+Progress::Progress(int left, int top, int width, int height, int start, int end)
+{
+	m_view = new ProgressView(left, top, width, height);
+	m_controller = new ProgressController(left, top, width, height);
+	m_model = new ProgressModel(start, end);
+
+	m_view->set_model(m_model);
+	m_controller->set_model(m_model);
+}
+
+
+Progress::~Progress()
+{
+	if (m_view)
+	{
+		delete m_view;
+		m_view = nullptr;
+	}
+
+	if (m_controller)
+	{
+		delete m_controller;
+		m_controller = nullptr;
+	}
+
+	if (m_model)
+	{
+		delete m_model;
+		m_model = nullptr;
+	}
+}
+
+
+// 绘制
+void Progress::draw(Gdiplus::Graphics &g)
+{
+	m_view->draw(g);
+}
+
+
+// 击中测试
+bool Progress::hit_test(int x, int y) const
+{
+	return m_controller->hit_test(x, y);
+}
+
+
+// 光标移出
+void Progress::mouse_move_out()
+{
+	m_controller->mouse_move_out();
+}
+
+
+// 光标移入
+void Progress::mouse_move_in()
+{
+	m_controller->mouse_move_in();
+}
+
+
+// 鼠标左键按下
+void Progress::Progress::left_button_down()
+{
+	m_controller->left_button_down();
+}
+
+
+// 鼠标左键弹起
+void Progress::left_button_up()
+{
+	m_controller->left_button_up();
+}
+
+
+// 光标移动消息
+void Progress::mouse_move_over()
+{
+	m_controller->mouse_move_over();
+}
---
+++ SLPlayer/Progress.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "AbstractControl.h"
+#include "ProgressView.h"
+#include "ProgressController.h"
+#include "ProgressModel.h"
+
+class Progress :
+	public AbstractControl
+{
+public:
+	Progress(int left, int top, int width, int height, int start = 0, int end = 100);
+	virtual ~Progress();
+
+	// 绘制
+	void draw(Gdiplus::Graphics &g) override;
+
+	// 击中测试
+	bool hit_test(int x, int y) const override;
+	// 光标移出
+	void mouse_move_out() override;
+	// 光标移入
+	void mouse_move_in() override;
+	// 鼠标左键按下
+	void left_button_down() override;
+	// 鼠标左键弹起
+	void left_button_up() override;
+
+	// 光标移动消息
+	void mouse_move_over();
+
+private:
+	ProgressView *m_view = nullptr;
+	ProgressController *m_controller = nullptr;
+	ProgressModel *m_model = nullptr;
+};
+
