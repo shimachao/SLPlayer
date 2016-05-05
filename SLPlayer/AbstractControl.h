b4e6a6391b3108148147df3d7f4779a3a1362883
chao <531236305@qq.com>
2016-05-05 10:49:40 +0800

    修正手误。

---
+++ SLPlayer/AbstractControl.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "AbstractView.h"
+#include "AbstractController.h"
+
+class AbstractControl:public AbstractView,public AbstractController
+{
+public:
+	AbstractControl() = default;
+	~AbstractControl() = default;
+
+private:
+
+};
