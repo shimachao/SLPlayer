6e0c159fe25c8448402a6c55a91e15592330892f
chao <531236305@qq.com>
2016-05-05 11:19:58 +0800

    添加Button类。
     - Button类组合了ButtonView,ButtonController,ButtonModel。其构造函数相当于一个工厂函数，负责创建后三者。

---
+++ SLPlayer/Button.cpp
@@ -0,0 +1,76 @@
+#include "Button.h"
+
+
+
+Button::Button(int left, int top, int width, int height)
+{
+	m_view = new ButtonView(left, top, width, height);
+	m_controller = new ButtonController(left, top, width, height);
+	m_model = new ButtonModel();
+
+	m_view->set_model(m_model);
+	m_controller->set_model(m_model);
+}
+
+
+Button::~Button()
+{
+	if (m_view)
+	{
+		delete m_view;
+		m_view = nullptr;
+	}
+
+	if (m_controller)
+	{
+		delete m_controller;
+		m_controller = nullptr;
+	}
+
+	if (m_model)
+	{
+		delete m_model;
+		m_model = nullptr;
+	}
+}
+
+
+// 绘制
+void Button::draw(Gdiplus::Graphics &g)
+{
+	m_view->draw(g);
+}
+
+// 击中测试
+bool Button::hit_test(int x, int y) const
+{
+	return m_controller->hit_test(x, y);
+}
+
+
+// 光标移出
+void Button::mouse_move_out()
+{
+	m_controller->mouse_move_out();
+}
+
+
+// 光标移入
+void Button::mouse_move_in()
+{
+	m_controller->mouse_move_in();
+}
+
+
+// 鼠标左键按下
+void Button::left_button_down()
+{
+	m_controller->left_button_down();
+}
+
+
+// 鼠标左键弹起
+void Button::left_button_up()
+{
+	m_controller->left_button_up();
+}
---
+++ SLPlayer/Button.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "AbstractControl.h"
+#include "ButtonView.h"
+#include "ButtonController.h"
+#include "ButtonModel.h"
+
+class Button :
+	public AbstractControl
+{
+public:
+	Button(int left, int top, int width, int height);
+	virtual ~Button();
+
+	// 绘制
+	void draw(Gdiplus::Graphics &g) override;
+
+	// 击中测试
+	bool hit_test(int x, int y) const override;
+	// 光标移出
+	void mouse_move_out() override;
+	// 光标移入
+	void mouse_move_in() override;
+	// 鼠标左键按下
+	void left_button_down() override;
+	// 鼠标左键弹起
+	void left_button_up() override;
+
+private:
+	ButtonView *m_view = nullptr;
+	ButtonController *m_controller = nullptr;
+	ButtonModel *m_model = nullptr;
+};
+
