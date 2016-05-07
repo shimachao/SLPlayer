3bf68b6d6313fcc93bb6d4e489e199d4198c105f
chao <531236305@qq.com>
2016-05-07 11:31:04 +0800

    实现显示状态的绘制函数。

--- SLPlayer/LabelView.cpp
+++ SLPlayer/LabelView.cpp
@@ -34,17 +34,14 @@ void LabelView::set_pos(int left, int top, int width, int height)
 // 绘制
 void LabelView::draw(Gdiplus::Graphics &g)
 {
-	if (m_model)
+	auto state = m_model->get_state();
+	if (state == LabelModel::State::DISPLAY)
 	{
-		auto state = m_model->get_state();
-		if (state == LabelModel::State::DISPLAY)
-		{
-			display_draw(g);
-		}
-		else if (state == LabelModel::State::EDIT)
-		{
-			edit_draw(g);
-		}
+		display_draw(g);
+	}
+	else if (state == LabelModel::State::EDIT)
+	{
+		edit_draw(g);
 	}
 }
 
@@ -52,7 +49,19 @@ void LabelView::draw(Gdiplus::Graphics &g)
 // 显示状态的绘制
 void LabelView::display_draw(Gdiplus::Graphics &g)
 {
-	// todo
+	// 绘制背景颜色
+	Gdiplus::SolidBrush bgBrush(Gdiplus::Color::Yellow);
+	g.FillRectangle(&bgBrush, m_left, m_top, m_width, m_height);
+
+	// 绘制文字
+	Gdiplus::FontFamily fontFamily(L"微软雅黑");
+	Gdiplus::Font font(&fontFamily, 14, Gdiplus::FontStyleRegular, Gdiplus::UnitPixel);
+	Gdiplus::SolidBrush textBrush(Gdiplus::Color::Black);
+	Gdiplus::StringFormat stringFormat;
+	stringFormat.SetAlignment(Gdiplus::StringAlignmentCenter);
+	stringFormat.SetLineAlignment(Gdiplus::StringAlignmentCenter);
+	Gdiplus::RectF rectF(m_left, m_top, m_width, m_height);
+	g.DrawString(m_model->get_text().c_str(), -1, &font, rectF, &stringFormat, &textBrush);
 }
 
 // 编辑状态的绘制
