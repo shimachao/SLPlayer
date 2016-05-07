36e2b46f4f67ae712558f096b7185725e91068d7
chao <531236305@qq.com>
2016-05-07 09:15:51 +0800

    添加设置绘制范围方法。绘制范围是控件在界面上显示的大小范围。

--- SLPlayer/AbstractView.h
+++ SLPlayer/AbstractView.h
@@ -8,6 +8,8 @@ public:
 	AbstractView();
 	virtual ~AbstractView();
 
+	// …Ë÷√ªÊ÷∆∑∂Œß
+	virtual void set_pos(int left, int top, int width, int height) = 0;
 	// ªÊ÷∆
 	virtual void draw(Gdiplus::Graphics &g) = 0;
 };
