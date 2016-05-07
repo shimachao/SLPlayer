7912756e6fbdafa90e2c6db45595262026566c1b
chao <531236305@qq.com>
2016-05-07 20:24:20 +0800

    修正方法实现

--- SLPlayer/ButtonModel.cpp
+++ SLPlayer/ButtonModel.cpp
@@ -53,5 +53,5 @@ ButtonModel::State ButtonModel::get_state() const
 // 连接事件和动作
 void ButtonModel::connect(Event e, std::function<void()> action)
 {
-	m_eventToAction[Event::CLICK] = action;
+	m_eventToAction[e] = action;
 }
